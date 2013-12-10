ced9510e6a0c61c4006bbc979e9befe6b6905332
ronaldpatino <ronaldpatino@gmail.com>
2013-12-10 14:12:03 -0500

    Ajustes nuevas imagenes

--- page--front.tpl.php
+++ page--front.tpl.php
@@ -30,7 +30,7 @@
     </div>
 
     <div class="barra-main">
-        <img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/consulte-planilla.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/mapa-sitio.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/contacto.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/comentarios.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/quejas-reclamos.png">
+        <img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/mapa-sitio.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/actualize-datos.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/contacto.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/comentarios.png">
     </div>
     <br/>
     <div class="row">
@@ -54,7 +54,7 @@
     </div>
     <div class="row">
         <div class="col-xs-12">
-            <img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/ecuador-ama-la-vida.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/presidencia-ecuador.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/ministerio-energia.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/ministerio-coordinador.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/revolucion-ciudadana.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/youtube.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/facebook.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/twitter.png">
+            <img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/ecuador-ama-la-vida.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/presidencia-ecuador.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/ministerio-energia.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/ministerio-coordinador.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/facebook.png"><img src="<?php print base_path() . drupal_get_path('theme', 'EG'); ?>/img/twitter.png">
         </div>
     </div>
 
